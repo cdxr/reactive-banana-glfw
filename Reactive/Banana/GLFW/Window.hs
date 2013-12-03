3b73fdb6e2b15eb8a06a5ef9acbe06be2c2cfe61
Craig Roche <cdxr01@gmail.com>
2013-12-03 15:53:58 -0500

    added transWindowSource

--- Reactive/Banana/GLFW/Window.hs
+++ Reactive/Banana/GLFW/Window.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Reactive.Banana.GLFW.Window
 (
     WindowSource(..),
     bindWindowSource,
+    transWindowSource,
 )
 where
 
@@ -29,22 +32,22 @@ data WindowSource f = WindowSource
     }
 
 
--- | Create an `AddHandler'` for a callback in the shape provided by GLFW-b
-handleCallback
-    :: ((a -> IO ()) -> cb)
-    -> (Maybe cb -> IO ())
-    -> IO (AddHandler' a)
-handleCallback f cb = do
-    (ah, fire) <- newAddHandler
-    liftIO $ cb $ Just $ f fire
-    return $ AddHandler' ah
-
-
-hc1 = handleCallback $ \fire _ -> fire ()
-hc2 = handleCallback $ \fire _ a -> fire a
-hc3 = handleCallback $ \fire _ a b -> fire (a, b)
-hc4 = handleCallback $ \fire _ a b c -> fire (a, b, c)
-hc5 = handleCallback $ \fire _ a b c d -> fire (a, b, c, d)
+-- | @transWindowSource n w@ is the `WindowSource` resulting from applying
+-- the natural transformation @n@ to each component of @w@.
+transWindowSource :: (forall a. f a -> g a) -> WindowSource f -> WindowSource g
+transWindowSource f w = w
+    { refresh     = f (refresh w)
+    , close       = f (close w)
+    , focus       = f (focus w)
+    , iconify     = f (iconify w)
+    , position    = f (position w)
+    , size        = f (size w)
+    , key         = f (key w)
+    , char        = f (char w)
+    , mouse       = f (mouse w)
+    , cursorPos   = f (cursorPos w)
+    , cursorEnter = f (cursorEnter w)
+    }
 
 
 bindWindowSource :: GLFW.Window -> IO (WindowSource AddHandler')
@@ -63,3 +66,22 @@ bindWindowSource w = WindowSource
     <*> hc3 (GLFW.setCursorPosCallback w)
     <*> (fmap (== CursorState'InWindow)
         <$> hc2 (GLFW.setCursorEnterCallback w))
+
+
+-- | Create an `AddHandler'` for a callback in the shape provided by GLFW-b
+handleCallback
+    :: ((a -> IO ()) -> cb)
+    -> (Maybe cb -> IO ())
+    -> IO (AddHandler' a)
+handleCallback f cb = do
+    (ah, fire) <- newAddHandler
+    liftIO $ cb $ Just $ f fire
+    return $ AddHandler' ah
+
+
+hc1 = handleCallback $ \fire _ -> fire ()
+hc2 = handleCallback $ \fire _ a -> fire a
+hc3 = handleCallback $ \fire _ a b -> fire (a, b)
+hc4 = handleCallback $ \fire _ a b c -> fire (a, b, c)
+hc5 = handleCallback $ \fire _ a b c d -> fire (a, b, c, d)
+
