c32b29b2f5fb43fbde6df1b5cbe1888a122942bf
Craig Roche <cdxr01@gmail.com>
2013-12-03 15:41:51 -0500

    added AddHandler' type

---
+++ Reactive/Banana/GLFW/AddHandler.hs
@@ -0,0 +1,25 @@
+module Reactive.Banana.GLFW.AddHandler where
+
+import Data.Monoid
+
+import Reactive.Banana
+import Reactive.Banana.Frameworks
+
+
+-- | A concrete `AddHandler`. This is useful for its `Functor` and `Monoid`
+-- instances.
+newtype AddHandler' a = AddHandler' { registerCallback :: AddHandler a }
+
+instance Functor AddHandler' where
+    fmap f ah = AddHandler' $ \cb -> registerCallback ah (cb . f)
+
+instance Monoid (AddHandler' a) where
+    mempty = AddHandler' $ \_ -> return (return ())
+    a `mappend` b = AddHandler' $ \cb -> do
+        da <- registerCallback a cb
+        db <- registerCallback b cb
+        return $ db >> da
+        
+
+fromAddHandler' :: Frameworks t => AddHandler' a -> Moment t (Event t a)
+fromAddHandler' = fromAddHandler . registerCallback
